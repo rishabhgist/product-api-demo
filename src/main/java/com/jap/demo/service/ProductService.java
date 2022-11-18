f2ad923c4da0b05b3d2d2813e3004c8e4091f811
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 17:18:55 +0530

    added IProductService Implements

--- src/main/java/com/jap/demo/service/ProductService.java
+++ src/main/java/com/jap/demo/service/ProductService.java
@@ -1,15 +1,28 @@
 package com.jap.demo.service;
 
+import com.jap.demo.domain.Product;
 import com.jap.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProductService {
+public class ProductService implements IProductService {
     private ProductRepository productRepository;
 
     @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
     }
 }
