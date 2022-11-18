9adf2f6cacf238f7cff06a5663ad4ba563560ed1
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 17:16:28 +0530

    added save and getAll abstract methods

--- src/main/java/com/jap/demo/service/IProductService.java
+++ src/main/java/com/jap/demo/service/IProductService.java
@@ -1,4 +1,11 @@
 package com.jap.demo.service;
 
+import com.jap.demo.domain.Product;
+
+import java.util.List;
+
 public interface IProductService {
+    Product save(Product product);
+
+    List<Product> getAll();
 }
