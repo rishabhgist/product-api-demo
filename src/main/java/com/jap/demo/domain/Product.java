34291e0da582c6de0cc5e1be3fa7a004e07470ca
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 17:12:30 +0530

    added fields, constructors and getters and setters in Product Domain

--- src/main/java/com/jap/demo/domain/Product.java
+++ src/main/java/com/jap/demo/domain/Product.java
@@ -1,4 +1,45 @@
 package com.jap.demo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String manufacturer;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String manufacturer) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 }
