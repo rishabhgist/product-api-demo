8ee31db6e07ad58f9cfde3760d918fd62f075336
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 17:28:16 +0530

    added getAll method

--- src/main/java/com/jap/demo/controller/ProductController.java
+++ src/main/java/com/jap/demo/controller/ProductController.java
@@ -5,11 +5,12 @@ import com.jap.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
     @Autowired
     public ProductController(ProductService productService) {
@@ -19,5 +20,10 @@ public class ProductController {
     @PostMapping("/post")
     public ResponseEntity<?> saveMovie(@RequestBody Product product) {
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 }
